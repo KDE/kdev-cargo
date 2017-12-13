1023fdbdd01806e02a4334b7d6c58d1e057c8a71
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-12-13 08:43:36 +0100

    Fix name of created i18n message catalog

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg | grep -v '/tests/'` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/kdevcustombuildsystem.pot
+$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/kdevcargo.pot
 rm -f rc.cpp
