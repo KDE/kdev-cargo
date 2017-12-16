05929874b606edeba31db80a863716e1c5f4ab43
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-12-16 10:56:26 +0100

    Extend FindCargo.cmake to support version checks

--- cmake/FindCargo.cmake
+++ cmake/FindCargo.cmake
@@ -1,7 +1,9 @@
 # Find the cargo executable
 #
 # Defines the following variables
+#  Cargo_FOUND      - True if the cargo executable was found
 #  Cargo_EXECUTABLE - path of the cargo executable
+#  Cargo_VERSION    - version number of cargo
 
 #=============================================================================
 # Copyright 2017 Friedrich W. H. Kossebau <kossebau@kde.org>
@@ -16,10 +18,29 @@
 
 find_program(Cargo_EXECUTABLE NAMES cargo)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cargo DEFAULT_MSG Cargo_EXECUTABLE)
+if (Cargo_EXECUTABLE)
+    execute_process(COMMAND "${Cargo_EXECUTABLE}" --version
+        OUTPUT_VARIABLE Cargo_VERSION_OUTPUT
+        ERROR_VARIABLE Cargo_VERSION_ERROR
+        RESULT_VARIABLE Cargo_VERSION_RESULT
+    )
+    if(NOT ${Cargo_VERSION_RESULT} EQUAL 0)
+        message(SEND_ERROR "Command \"${Cargo_EXECUTABLE} --version\" failed with output:\n${Cargo_VERSION_ERROR}")
+    else()
+        # TODO: support also nightly
+        string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
+            Cargo_VERSION "${Cargo_VERSION_OUTPUT}"
+        )
+    endif()
+endif()
 
-mark_as_advanced(Cargo_EXECUTABLE)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Cargo
+    REQUIRED_VARS Cargo_EXECUTABLE
+    VERSION_VAR Cargo_VERSION
+)
+
+mark_as_advanced(Cargo_EXECUTABLE Cargo_VERSION)
 
 set_package_properties(Cargo PROPERTIES
     DESCRIPTION "The Rust package manager"
